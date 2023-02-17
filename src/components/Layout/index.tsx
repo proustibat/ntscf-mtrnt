b267e6afd657e14c121d6b4987b55c3eed91c343
prousitbat <jennifer.proust@gmail.com>
2023-02-17 12:18:22 +0100

    feat(layout): add it with global head
    Use the layout to components on every pages. Don't forget to use it in pages!

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,19 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+import { ReactNode } from 'react';
+import Head from 'next/head';
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Online score: play classical sheet with Metronaut"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,18 @@
 import Head from 'next/head';
+import Layout from '@/components/Layout';
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Pachelbel - Canon [Metronaut by Antescofo]</title>
-        <meta
-          name="description"
-          content="Online violin score: play classical sheet with Metronaut"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <section>
         <h1>Hello world</h1>
-      </main>
-    </>
+      </section>
+
+      <section>
+        <p>Page content</p>
+      </section>
+    </Layout>
   );
 }
