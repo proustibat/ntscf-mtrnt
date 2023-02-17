37d1c6b0bbbe5f8c8de1f958a166b16447f929ab
prousitbat <jennifer.proust@gmail.com>
2023-02-17 12:07:32 +0100

    style(config): eslint & prettier
    Add configs for linters, also apply rules on existing files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import Head from 'next/head'
+import Head from 'next/head';
 export default function Home() {
   return (
     <>
       <Head>
         <title>Pachelbel - Canon [Metronaut by Antescofo]</title>
-        <meta name="description" content="Online violin score: play classical sheet with Metronaut" />
+        <meta
+          name="description"
+          content="Online violin score: play classical sheet with Metronaut"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-          <h1>
-            Hello world
-          </h1>
+        <h1>Hello world</h1>
       </main>
     </>
-  )
+  );
 }
